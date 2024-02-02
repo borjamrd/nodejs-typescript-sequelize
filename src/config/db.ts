b43a75c646164860685d20da65b81b96dfef7501
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2024-02-02 10:03:49 +0100

    db connection

---
+++ src/config/db.ts
@@ -0,0 +1,39 @@
+import { Sequelize } from "sequelize";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+class Database {
+  public sequelize: Sequelize | undefined;
+
+  private POSTGRES_DB = process.env.POSTGRES_DB as string;
+  private POSTGRES_HOST = process.env.POSTGRES_HOST as string;
+  private POSTGRES_PORT = process.env.POSTGRES_PORT as unknown as number;
+  private POSTGRES_USER = process.env.POSTGRES_USER as unknown as string;
+  private POSTGRES_PASSWORD = process.env
+    .POSTGRES_PASSWORD as unknown as string;
+
+  constructor() {
+    this.connectToPostgreSQL();
+  }
+  private async connectToPostgreSQL() {
+    this.sequelize = new Sequelize({
+      database: this.POSTGRES_DB,
+      username: this.POSTGRES_USER,
+      password: this.POSTGRES_PASSWORD,
+      host: this.POSTGRES_HOST,
+      port: this.POSTGRES_PORT,
+      dialect: "postgres",
+    });
+
+    await this.sequelize
+      .authenticate()
+      .then(() => {
+        console.log("PostgreSQL: connection succesfully");
+      })
+      .catch((error: Error) => {
+        console.error("Error PostgreSQL:", error);
+      });
+  }
+}
+
+export default Database;
